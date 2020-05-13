0989b3f2dc3b59714e5b90126ca5007e8781ce5d
FCastillo-91 <fionacastillo@rentalcars.com>
2020-05-13 11:46:48 +0100

    Reset Time display

--- App.js
+++ App.js
@@ -9,13 +9,13 @@ let timeSelected = 0;
 const playBtn = document.querySelector(".play");
 const timeDisplay = document.querySelector(".time-display");
 const sounds = document.querySelectorAll(".sound-picker button");
-const timeSelect = document.querySelectorAll(".time-select button");
 const outline = document.querySelector(".moving-outline circle");
 const outlineLength = outline.getTotalLength();
 outline.style.strokeDasharray = outlineLength;
 outline.style.strokeDashoffset = outlineLength;
 
 const timeSelector = () => {
+  const timeSelect = document.querySelectorAll(".time-select button");
   timeSelect.forEach((btn) => {
     btn.addEventListener("click", () => {
       updateTimeDisplay(btn);
@@ -26,8 +26,12 @@ const timeSelector = () => {
 const updateTimeDisplay = (btn) => {
   timeSelected = btn.getAttribute("data-time");
   meditationTime = timeSelected;
-  timeDisplay.textContent = formatTime(timeSelected);
+  timeDisplay.textContent = formatTime(meditationTime);
   showPlayBtn();
+  soundPauser();
+  vidPauser();
+  stopTimer();
+  setCircle();
 };
 
 const pressPlay = () => {
@@ -35,13 +39,12 @@ const pressPlay = () => {
     if (!meditationTime) {
       alertify.alert("Please select a time", function () {});
       return null;
-    } else{
+    } else {
       showPauseBtn();
       soundPlayer();
       videoPlayer();
       startTimer();
     }
-
   });
 };
 
@@ -61,7 +64,7 @@ const startTimer = () => {
     if (timeLeft >= 0) {
       timeDisplay.innerHTML = formatTime(timeLeft);
       setCircle(
-          Math.floor(outlineLength - (timeLeft / meditationTime) * outlineLength)
+        Math.floor(outlineLength - (timeLeft / meditationTime) * outlineLength)
       );
     } else {
       soundPauser();
